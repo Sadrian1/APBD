6a50f3908bb24630be8eb8cc22705ef160d430cf
Sadrian1 <s25910@pjwstk.edu.pl>
2024-03-23 23:17:43 +0100

    Modyfikacja 3

--- Apka/Program.cs
+++ Apka/Program.cs
@@ -1,8 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Haha");
-if (20 > 18) 
-{
-    Console.WriteLine("20 is greater than 18");
-}
+
