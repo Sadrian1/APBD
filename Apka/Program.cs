174622b6ba6e9340ef7407b69c83c001687991bf
Sadrian1 <s25910@pjwstk.edu.pl>
2024-03-23 23:24:59 +0100

    Add CalculateAverage method to compute the average of an int array

--- Apka/Program.cs
+++ Apka/Program.cs
@@ -1,4 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+public static class ArrayUtils
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Array cannot be null or empty.", nameof(numbers));
+        }
 
+        long sum = 0;
+        foreach (var num in numbers)
+        {
+            sum += num;
+        }
+        return (double)sum / numbers.Length;
+    }
+}
